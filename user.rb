608b547c4eaa2e9be5e4b0406b64b7e440513211
mizuki-iio <mizuki.iio@di-v.co.jp>
2020-10-30 17:16:43 +0900

    attr_accessor

--- user.rb
+++ user.rb
@@ -1,4 +1,8 @@
 class User
+  # ゲッター・セッター（10~18行目）をコメントアウトして、以下の4行目をコメントアウト外しても同じ挙動
+  # attr_accessorがゲッターとセッターを定義している
+  # attr_accessor :money
+
   # 初期化
   def initialize(money)
     @money = money
